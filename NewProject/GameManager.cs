a7b17858cfa4d94fa570555ef67dc1f51a3facc2
alessia.lionetto <alessia.lionetto@WINAP5J7TK9AH8S>
2021-11-18 16:07:43 +0100

    Add project files.

---
+++ NewProject/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject
+{
+    internal class GameManager
+    {
+        public static void getNumber()
+        {
+            Console.WriteLine("inserisci un numero tra 2 e 12");
+            Random random = new Random();
+            int dado1 = random.Next(1, 7);
+            int dado2 = random.Next(1, 7);
+            if(num<12 && num > 2)
+            {
+                int somma=dado1 + dado2;
+                Console.WriteLine("la somma dei 2 dadi è " + somma);
+            }
+            else
+            {
+                Console.WriteLine("il numero inserito non è  corretto");
+            }
+
+
+        }
+        public static void Control()
+        {
+
+        }
+    }
+}
---
+++ NewProject/Program2.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//ES1
+//Scrivere un programma che simula un gioco di lancio dei dadi.
+
+//L'utente inserisce un numero tra 2 e 12.
+
+//Il computer lancia 2 dadi, cioè sorteggia 2 numeri random tra 1 e 6.
+//Se la somma dei due numeri random è pari al numero scelto dall'utente,
+//l'utente vince.
+//Se l'utente vince, stampare 'hai vinto', altrimenti 'hai perso'.
+//Finita la partita l'utente deve poter rigiocare.
+//Requisiti:
+//verificare che l'utente inserisca un intero e che sia compreso tra 2
+//e 12.
+//se la verifica non va a buon fine, l'utente deve potere inserire
+//nuovamente il numero.
+//Suggerimento:
+//Creare una classe con i metodi che servono da chiamare nella classe
+//program:
+//per esempio un metodo per l'inserimento dei numeri e la verifica,
+//uno per il controllo della vittoria.
+
+int num = NewProject.GameManager.getNumber();
+
+string s=Console.ReadLine();
+//ES2
+//Stampare un messaggio di benvenuto al gioco Morra Cinese.
+//Chiedere il NOME dell'utente quindi stampare un messaggio del tipo "Ok NOME, iniziamo a giocare".
+
+
+
+//Il computer sceglie (in modo random) una tra le possibili opzioni tra carta, sasso o forbice.
+//Avvisa l'utente che ha effettuato la sua scelta quindi invita l'utente a fare lo stesso cioè
+//a scegliere una delle 3 opzioni tra quelle disponibili per poter giocare contro di lui.
+
+
+
+//Dopo la scelta dell'utente sarà quindi "valutato" il vincitore in base al confronto tra le opzioni scelte.
+//Stampare a video un messaggio all'utente che dica se ha vinto, ha perso oppure c'è stato un pareggio.
+
+
+
+//Ricordiamo che:
+//Sasso vince su Forbice
+//Carta vince su Sasso
+//Forbice vince su Carta
+
+
+
+//Al termine del gioco si richiede all'utente se desidera giocare ancora contro il computer.
+//In caso affermativo si ricomincia a giocare,
+//altrimenti si esce dal gioco e deve essere mostrato un messaggio di arrivederci.
